e1c7ef4d80082e67dd28c588f588ca23ab45eadd
squinteroa <squinteroa@unicesar.edu.co>
2021-09-22 15:45:28 -0500

    html y css listos

---
+++ index.js
@@ -0,0 +1,8 @@
+const displayValorAnterior = document.getElementById('valor-anterior');/*hace referencia al valor anterior de index.html*/ 
+const displayValorAnterior = document.getElementById('valor-actual');/*hace referencial al valor actual de index.html*/
+const botonesNumeros = document.querySelectorAll('.numero');/*hace referencia a la clase numero de index.html*/
+const botonesOperadores = document.querySelectorAll('.operador');/*hace referencia a la clase operador*/
+
+
+const calculadora = new Calculadora();
+console.log(Calculadora.sumar(2,3))
