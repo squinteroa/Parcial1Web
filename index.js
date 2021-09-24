8ebcb03c6614c2c34acfbca60304cf328590ec47
squinteroa <squinteroa@unicesar.edu.co>
2021-09-24 14:49:12 -0500

    jjdjdj

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const displayValorAnterior = document.getElementById('valor-anterior');/*hace referencia al valor anterior de index.html*/ 
-const displayValorAnterior = document.getElementById('valor-actual');/*hace referencial al valor actual de index.html*/
+const displayValorActual = document.getElementById('valor-actual');/*hace referencial al valor actual de index.html*/
 const botonesNumeros = document.querySelectorAll('.numero');/*hace referencia a la clase numero de index.html*/
-const botonesOperadores = document.querySelectorAll('.operador');/*hace referencia a la clase operadores20*/
+const botonesOperadores = document.querySelectorAll('.operador');/*hace referencia a la clase operadores*/
 
+const display= new Display(displayValorAnterior, displayValorActual);
 
 botonesNumeros.forEach(boton => {
     boton.addEventListener('click',() => display.agregarNumero(boton.innerHTML));
